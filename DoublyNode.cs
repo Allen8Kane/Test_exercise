01008659f6654cfcfec1703a0ec30555116f1292
Allen <mc_kind@mail.ru>
2020-02-16 22:41:51 +0600

    Сделал основной функционал

---
+++ DoublyNode.cs
@@ -0,0 +1,14 @@
+
+namespace Test_exercise
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Test_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray<string> myArray = new MyArray<string>();
+            myArray.AddFirst("Owner");
+            myArray.Add("Hello");
+            myArray.Add("World");
+            myArray.Add("!");
+            
+            Console.WriteLine("Количество элементов:" + myArray.Count());
+            
+            foreach (var item in myArray)
+            {
+                Console.Write(item + ' ');
+            }
+            Console.WriteLine();
+
+            myArray.Remove("Owner");
+
+            foreach (var item in myArray.BackEnumerator())
+            {
+                Console.Write(item + ' ');
+            }
+
+            var items = from i in myArray
+                  where i.Contains("Hello")
+                  select i;
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
